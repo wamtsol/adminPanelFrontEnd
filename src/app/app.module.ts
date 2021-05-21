719840ff0ccad4739e48eef7b91f1a6582eba14a
htcmafia <67920712+htcmafia@users.noreply.github.com>
2021-05-21 19:54:52 +0500

    ....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/searchBar/searchBar.component';
 import { TableComponent } from './components/table/table.component';
 import { TransactionComponent } from './screens/transaction/transaction.component';
 
@@ -18,6 +19,7 @@ import { TransactionComponent } from './screens/transaction/transaction.componen
     TransactionComponent,
 
     TableComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
