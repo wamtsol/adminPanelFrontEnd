6fd05d40f695860c1557a427e50a76f507559fbf
htcmafia <67920712+htcmafia@users.noreply.github.com>
2021-05-24 20:27:28 +0500

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'adminPanel';
   showFiller = false;
+  showText: boolean = true;
   typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
